0701859f43004b4eecea45be1ac427c8fe29825b
Tom Shea <tom@shea.at>
2015-01-08 17:16:30 +0000

    Log connection info using debug

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var Game = {};
+var log = require('debug')("overwatch:game");
 
 Game.tick = function(state) {
 };
@@ -8,16 +9,27 @@ Game.create = function(id) {
   game.id = id;
   game.agent = null;
   game.overwatch = null;
+  log("new game @%s", id);
   return game;
 };
 
 Game.registerNewSocket = function(state, socket) {
+  var username = socket.request.session.passport.user;
+  log("register new socket for %s @%s", username, state.id);
   socket.on('disconnect', function() {
-    console.log("Game client disconnected");
+    log("client disconnected");
   });
   socket.on('ping', function() {
     socket.emit('pong', Date.now());
   });
+  if(username == state.agent) {
+    state.agentSocket = socket;
+    log("user %s is agent @%s", username, state.id);
+  }
+  if(username == state.overwatch) {
+    state.overwatchSocket = socket;
+    log("user %s is overwatch @%s", username, state.id);
+  }
 };
 
 
